1317db7cdddd361fba11042d4682ba2c4ce622ff
williambl <william@williambl.com>
2018-12-04 17:03:25 +0000

    Make VLC start playing if it's not when new media is enqueued

--- playMediaInFolder.sh
+++ playMediaInFolder.sh
@@ -7,4 +7,8 @@ inotifywait -m $QUEUE_FOLDER -e create -e moved_to --format '%f' |
     while read FILE; do
         mv $QUEUE_FOLDER/$FILE $PLAYED_FOLDER/$FILE
         vlc --one-instance --playlist-enqueue $PLAYED_FOLDER/$FILE
+
+        if [[ ! $(qdbus org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus) = "Playing" ]]; then
+            dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+        fi
     done
