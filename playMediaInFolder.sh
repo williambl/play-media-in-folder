86003fa187836161059f983782131970d862c7bb
williambl <william@williambl.com>
2018-12-04 17:38:25 +0000

    Add quotes as to not break if there are special characters in filenames

--- playMediaInFolder.sh
+++ playMediaInFolder.sh
@@ -4,12 +4,12 @@ PLAYED_FOLDER=playedFolder
 echo Starting VLC Media Player.
 vlc --one-instance >/dev/null 2>&1 &
 
-inotifywait -m $QUEUE_FOLDER -e create -e moved_to --format '%f' |
+inotifywait -m "$QUEUE_FOLDER" -e create -e moved_to --format '%f' |
     while read FILE; do
         echo Enqueueing $FILE
 
-        mv $QUEUE_FOLDER/$FILE $PLAYED_FOLDER/$FILE
-        vlc --one-instance --playlist-enqueue $PLAYED_FOLDER/$FILE
+        mv "$QUEUE_FOLDER/$FILE" "$PLAYED_FOLDER/$FILE"
+        vlc --one-instance --playlist-enqueue "$PLAYED_FOLDER/$FILE"
 
         if [[ ! $(qdbus org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus) = "Playing" ]]; then
             dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
