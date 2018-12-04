6eb46615bf4a5e84c8e5a615367b9be56073ddc1
williambl <william@williambl.com>
2018-12-04 17:18:57 +0000

    Add some echos to let the user know what's happenning

--- playMediaInFolder.sh
+++ playMediaInFolder.sh
@@ -1,10 +1,13 @@
 QUEUE_FOLDER=queueFolder
 PLAYED_FOLDER=playedFolder
 
+echo Starting VLC Media Player.
 vlc --one-instance >/dev/null 2>&1 &
 
 inotifywait -m $QUEUE_FOLDER -e create -e moved_to --format '%f' |
     while read FILE; do
+        echo Enqueueing $FILE
+
         mv $QUEUE_FOLDER/$FILE $PLAYED_FOLDER/$FILE
         vlc --one-instance --playlist-enqueue $PLAYED_FOLDER/$FILE
 
