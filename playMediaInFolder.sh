9c4d6521e42ee6f2bab6664a2db9b1e519a38b1f
williambl <william@williambl.com>
2018-12-04 16:47:23 +0000

    Make only one vlc instance be created

--- playMediaInFolder.sh
+++ playMediaInFolder.sh
@@ -1,8 +1,10 @@
 QUEUE_FOLDER=queueFolder
 PLAYED_FOLDER=playedFolder
 
+vlc --one-instance >/dev/null 2>&1 &
+
 inotifywait -m $QUEUE_FOLDER -e create -e moved_to --format '%f' |
     while read FILE; do
         mv $QUEUE_FOLDER/$FILE $PLAYED_FOLDER/$FILE
-        vlc $PLAYED_FOLDER/$FILE
+        vlc --one-instance --playlist-enqueue $PLAYED_FOLDER/$FILE
     done
